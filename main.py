27e01bc7ed893dc77e747ce382b300e7c2a22507
chahak1707 <pahujamanni1978@gmail.com>
2026-06-26 22:09:14 +0530

    Module5-lesson2

---
+++ main.py
@@ -0,0 +1,12 @@
+class fruit:
+    def __init__(self,name,color):
+        self.name = name
+        self.color = color
+
+apple = fruit('Apple','Red')
+print(apple.name)
+print(apple.color)
+
+watermelon = fruit('Watermelon','Green')
+print(watermelon.name)
+print(watermelon.color)
